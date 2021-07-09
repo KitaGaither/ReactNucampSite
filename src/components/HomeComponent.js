df58ffc593285f19d8403ccc5e76a3a701e93179
Lakita Gaither <lakitagaither@yahoo.com>
2021-07-08 19:33:22 -0500

    Fetch from Server

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,39 +1,45 @@
 import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { baseUrl } from '../shared/baseUrl';
 import { Loading } from './LoadingComponent';
 
-function RenderCard({item, isLoading, errMess}) {
+function RenderCard({ item, isLoading, errMess }) {
   if (isLoading) {
-      return <Loading />;
+    return <Loading />;
   }
   if (errMess) {
-      return <h4>{errMess}</h4>;
+    return <h4>{errMess}</h4>;
   }
-    return (
-        <Card>
-            <CardImg src={item.image} alt={item.name} />
-            <CardBody>
-                <CardTitle>{item.name}</CardTitle>
-                <CardText>{item.description}</CardText>
-            </CardBody>
-        </Card>
-    );
+  return (
+    <Card>
+      <CardImg src={baseUrl + item.image} alt={item.name} />
+      <CardBody>
+        <CardTitle>{item.name}</CardTitle>
+        <CardText>{item.description}</CardText>
+      </CardBody>
+    </Card>
+  );
 }
 
 function Home(props) {
-    return (
-        <div className="container">
-            <div className="row">
-                <div className="col-md m-1">
-                <RenderCard
-                  item={props.campsite}
-                  isLoading={props.campsitesLoading}
-                  errMess={props.campsitesErrMess}
-                />
-                </div>
-            </div>
+  return (
+    <div className='container'>
+      <div className='row'>
+        <div className='col-md m-1'>
+          <RenderCard
+            item={props.campsite}
+            isLoading={props.campsitesLoading}
+            errMess={props.campsitesErrMess}
+          />
+          <RenderCard
+            item={props.promotion}
+            isLoading={props.promotionLoading}
+            errMess={props.promotionErrMess}
+          />
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
-export default Home;   
+export default Home;
